dd872e166a149908b5af2c98e0d9f4304752f0e5
Tayansam <samiraramo815@gmail.com>
2025-12-16 19:55:57 +0300

    html,css

--- script.js
+++ script.js
@@ -1,40 +1,38 @@
-const slider = document.getElementById('testimonialSlider');
-const prevBtn = document.getElementById('prevBtn');
-const nextBtn = document.getElementById('nextBtn');
+const slider = document.getElementById("testimonialSlider");
+const prevBtn = document.getElementById("prevBtn");
+const nextBtn = document.getElementById("nextBtn");
 
 const scrollAmount = 300; // Adjust scroll distance
 
-nextBtn.addEventListener('click', () => {
-  slider.scrollBy({ left: scrollAmount, behavior: 'smooth' });
+nextBtn.addEventListener("click", () => {
+  slider.scrollBy({ left: scrollAmount, behavior: "smooth" });
 });
 
-prevBtn.addEventListener('click', () => {
-  slider.scrollBy({ left: -scrollAmount, behavior: 'smooth' });
+prevBtn.addEventListener("click", () => {
+  slider.scrollBy({ left: -scrollAmount, behavior: "smooth" });
 });
 
-const burger = document.getElementById('burger');
-const mobileMenu = document.getElementById('mobileMenu');
-const mobileLinks = document.querySelectorAll('.mobile-link');
+const burger = document.getElementById("burger");
+const mobileMenu = document.getElementById("mobileMenu");
+const mobileLinks = document.querySelectorAll(".mobile-link");
 
-burger.addEventListener('click', () => {
-  mobileMenu.classList.toggle('active'); // toggle menu
-  burger.classList.toggle('toggle');     // toggle X animation
+burger.addEventListener("click", () => {
+  mobileMenu.classList.toggle("active"); // toggle menu
+  burger.classList.toggle("toggle"); // toggle X animation
 });
 
-
-
-  mobileLinks.forEach(link => {
-    link.addEventListener('click', () => {
-      mobileMenu.classList.remove('active');
-    });
+mobileLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    mobileMenu.classList.remove("active");
   });
+});
 
-
-  // Smooth scroll
-  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-      e.preventDefault();
-      document.querySelector(this.getAttribute('href'))
-        .scrollIntoView({ behavior: 'smooth' });
-    });
+// Smooth scroll
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document
+      .querySelector(this.getAttribute("href"))
+      .scrollIntoView({ behavior: "smooth" });
   });
+});
